128fc8a019f8df319656df6c4ecbd13436b9d00a
Germán Campagno <german@infinixsoft.com>
2016-03-23 11:00:48 -0300

    Settings screen UI done

---
+++ Paid to Go/SettingsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsViewController.swift
+//  Paid to Go
+//
+//  Created by MacbookPro on 23/3/16.
+//  Copyright © 2016 Infinixsoft. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: MenuContentViewController {
+    // MARK: - Outlets
+    
+    @IBOutlet weak var okButtonViewContainer: UIView!
+    
+    // MARK: - Variables and Constants
+    
+    
+    // MARK: - Super
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setNavigationBarVisible(true)
+        self.title = "Settings"
+        setNavigationBarGreen()
+        customizeNavigationBarWithMenu()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        initViews()
+    }
+    
+    // MARK: - Functions
+    
+    func initViews(){
+        okButtonViewContainer.round()
+    }
+    
+    // MARK: - Actions
+    
+
+}
