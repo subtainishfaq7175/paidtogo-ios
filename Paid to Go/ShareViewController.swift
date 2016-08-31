b0ab126bbdf774b02faba1c6cb0bfab1c640ce86
Nahuel <nahu_roldan@hotmail.com>
2016-08-31 17:33:49 -0300

    Working on Facebook share

--- Paid to Go/ShareViewController.swift
+++ Paid to Go/ShareViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Social
 import MessageUI
+import FBSDKShareKit
 
 class ShareViewController: ViewController, MFMailComposeViewControllerDelegate, UIDocumentInteractionControllerDelegate {
     
@@ -30,6 +31,7 @@ class ShareViewController: ViewController, MFMailComposeViewControllerDelegate, 
     var documentInteractor : UIDocumentInteractionController?
     
     var screenshot : UIImage?
+    var screenshotData : NSData?
     
     // MARK: - Super
     
@@ -134,11 +136,6 @@ class ShareViewController: ViewController, MFMailComposeViewControllerDelegate, 
             self.shareInstagram = CMDocumentShare()
         }
         
-//        guard let screenshot = self.screenShotMethod() as UIImage? else {
-//            print("NO SE PUDO SACAR EL SCREENSHOT")
-//            return
-//        }
-        
         self.shareInstagramImage(self.screenshot!)
         
 //        
@@ -165,11 +162,83 @@ class ShareViewController: ViewController, MFMailComposeViewControllerDelegate, 
     
     @IBAction func facebook(sender: AnyObject) {
         
-        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook){
-            let facebookSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
-            facebookSheet.setInitialText("Share on Facebook")
-            facebookSheet.addURL(NSURL(string: "www.paidtogo.com"))
-            self.presentViewController(facebookSheet, animated: true, completion: nil)
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            
+            // Image with no text
+            let content = FBSDKSharePhotoContent()
+            
+            if let image = self.screenshot {
+                let shareImage = FBSDKSharePhoto(image: image, userGenerated: true)
+                
+                content.photos = [shareImage]
+                
+                FBSDKShareDialog.showFromViewController(self, withContent: content, delegate: self)
+            }
+            
+            
+            /*
+            let content = FBSDKShareLinkContent()
+            if let url = NSURL(string: "https://www.paidtogo.com") {
+                content.contentURL = url
+            }
+            
+            content.contentTitle = "Paid to Go"
+            content.contentDescription = "\nHey! I just completed a pool, check out my stats"
+            
+            let dialog = FBSDKShareDialog()
+            if UIApplication.sharedApplication().canOpenURL(NSURL(string: "fbauth2://")!) {
+                dialog.mode = FBSDKShareDialogMode.Native
+            } else {
+                dialog.mode = FBSDKShareDialogMode.Browser
+            }
+            dialog.shareContent = content
+            dialog.delegate = self
+            dialog.fromViewController = self
+            dialog.show()
+            */
+            
+            // Share image 1: no funca
+//            let homeDirectory = NSHomeDirectory()
+//            let savePath = homeDirectory.stringByAppendingPathComponent("Documents/image.igo")
+//            
+//            print("savePath -> \(savePath)")
+//            
+//            if let image = self.screenshot {
+//                if let imgData = UIImageJPEGRepresentation(image, 0.1) {
+//                    do {
+//                        try imgData.writeToFile(savePath, options: NSDataWritingOptions.AtomicWrite)
+//                    } catch {
+//                        print("Something went wrong!")
+//                    }
+//                    
+//                    let theFileURL = NSURL(fileURLWithPath: savePath)
+//                    
+//                    content.imageURL = theFileURL
+//                }
+//            }
+            
+            // Share image 2: no funca
+//            if let screenshot = self.screenshot {
+//                if let data = UIImageJPEGRepresentation(screenshot, 0.1) {
+//                    let filePath = self.documentsPathForFileName("/image.jpg")
+//                    
+//                    do {
+//                        
+//                        let result = try Bool(data.writeToFile(filePath, options: NSDataWritingOptions.DataWritingAtomic))
+//                        print("result: \(result)")
+//                        
+//                        let filePathURL = NSURL(fileURLWithPath: filePath)
+//                        
+//                        content.imageURL = filePathURL
+//                        
+//                    }
+//                    catch let error as NSError {
+//                        print(error.localizedDescription)
+//                    }
+//                    
+//                }
+//            }
+            
         } else {
             let alert = UIAlertController(title: "Accounts", message: "Please login to a Facebook account to share.", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
@@ -195,7 +264,7 @@ class ShareViewController: ViewController, MFMailComposeViewControllerDelegate, 
         
         print("savePath -> \(savePath)")
         
-        let imgData = UIImageJPEGRepresentation(image, 1)
+        let imgData = UIImageJPEGRepresentation(image, 0.1)
         
         do {
             try imgData?.writeToFile(savePath, options: NSDataWritingOptions.AtomicWrite)
@@ -218,16 +287,41 @@ class ShareViewController: ViewController, MFMailComposeViewControllerDelegate, 
         }
     }
     
-    func screenShotMethod() -> UIImage? {
-        let layer = UIApplication.sharedApplication().keyWindow!.layer
-        let scale = UIScreen.mainScreen().scale
-        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+//    func screenShotMethod() -> UIImage? {
+//        let layer = UIApplication.sharedApplication().keyWindow!.layer
+//        let scale = UIScreen.mainScreen().scale
+//        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+//        
+//        layer.renderInContext(UIGraphicsGetCurrentContext()!)
+//        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//        return screenshot
+//    }
+    
+    func documentsPathForFileName(name: String) -> String {
         
-        layer.renderInContext(UIGraphicsGetCurrentContext()!)
-        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return screenshot
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+        return documentsPath.stringByAppendingString(name)
+    }
+    
+}
+
+// MARK: - Facebook Share Dialog Delegate -
+
+extension ShareViewController: FBSDKSharingDelegate {
+    
+    func sharer(sharer: FBSDKSharing!, didCompleteWithResults results: [NSObject: AnyObject]) {
+        print(results)
+    }
+    
+    func sharer(sharer: FBSDKSharing!, didFailWithError error: NSError!) {
+        print("sharer NSError")
+        print(error.description)
+    }
+    
+    func sharerDidCancel(sharer: FBSDKSharing!) {
+        print("sharerDidCancel")
     }
 }
 
